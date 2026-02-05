77ea6454b4ad8100155be1ac06b569be4d1e43b0
juwonthefirst <ajibolajuwon5@gmail.com>
2026-02-06 00:22:17 +0100

    fix: add remote patterns for new storage hostnames in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,16 @@ const nextConfig: NextConfig = {
         port: "8000",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "02facda14ba0c8cc2a640e2a039855a6.r2.cloudflarestorage.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "pub-28366e7f68a54624b152528f1964d965.r2.dev",
+        pathname: "/beep/**",
+      },
     ],
   },
 };
